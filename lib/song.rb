9d83071d83aeec106be8d54f2506e46866bb134d
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-13 18:00:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,8 @@ class Song
         parts = filename.split(" - ")
         artist_name = parts[0]
         song_name = parts[1].gsub(".mp3", "")
-
         song = self.new
-         song.name = song_name
+        song.name = song_name
          song.artist_name = artist_name
          song
       end
