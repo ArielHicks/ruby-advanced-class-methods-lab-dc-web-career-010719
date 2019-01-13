4f5f1e4b68758b0b07577e3af8e393339ad4e262
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-13 17:46:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,8 @@ class Song
       self.class.all << self
     end
 
-      def self.create
-        song = Song.new_by_name
-        song.save
-        song
-      end
-  end
+    def self.create
+ song = Song.new
+ song.save
+ song
+end
